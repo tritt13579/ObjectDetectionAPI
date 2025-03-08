5ba86f7d35f2b067d456b41f89b5b8ac42f04c9e
Thanh Tri <tritt13579@gmail.com>
2025-03-08 21:55:10 +0700

    add cors, allow all

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.middlewares.auth_middleware import AuthMiddleware
 from app.api.endpoints import model
 from app.api.endpoints import detect, detect_video, websocket
 
 app = FastAPI()
+
+# Cấu hình CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cho phép tất cả các domain truy cập
+    allow_credentials=True,  # Cho phép gửi cookie và thông tin xác thực
+    allow_methods=["*"],  # Cho phép tất cả các phương thức HTTP (GET, POST, PUT, DELETE, v.v.)
+    allow_headers=["*"],  # Cho phép tất cả các headers
+)
 
 # Thêm middleware
 app.add_middleware(AuthMiddleware)
